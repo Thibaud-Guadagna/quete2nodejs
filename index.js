3695009510e3305b331d8cd8c722b7098b4aee4c
thibaud.guadagna <thibaud.guadagna@gmail.com>
2025-03-24 11:02:11 +0100

    ajout des fichiers

---
+++ index.js
@@ -0,0 +1,8 @@
+const userName = process.env.USERNAME;
+const city = process.env.CITY;
+const language = process.env.LANGUAGE;
+
+console.log(`Hello, my name is ${userName}, I live in ${city} and I love ${language}.`);
+
+
+
